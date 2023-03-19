414fe0217e4b07fbbe6ff2bc22a942db4085eeec
Dharish8800 <128275767+Dharish8800@users.noreply.github.com>
2023-03-19 20:14:17 +0530

    Update ATMinterface.java

--- src/ATMinterface.java
+++ src/ATMinterface.java
@@ -187,7 +187,7 @@ class BankAccount {
 
 public class ATMinterface {
     public static void main(String[] args) {
-        BankAccount ba = new BankAccount("Huzefa", "1001");
+        BankAccount ba = new BankAccount("Harish", "1459");
         ba.checkId();
     }
 
